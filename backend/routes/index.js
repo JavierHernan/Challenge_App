ea373b4ffe559fcb39dbe5cefd89775ef8ff2ed3
Javier <javierLhernandez@yahoo.com>
2024-09-19 22:24:59 -0400

    added api router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,10 @@ const router = express.Router();
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+const apiRouter = require('./api'); //importing router from routes/api/index.js
+
+router.use('/api', apiRouter);
+
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
@@ -12,5 +16,15 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+//fetch request example to place in browser. Must get XSRF-TOKEN from Sources in Inspect
+// fetch('/api/test', {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//   },
+//   body: JSON.stringify({ hello: 'world' })
+// }).then(res => res.json()).then(data => console.log(data));
 
 module.exports = router;
