584b4a13fa29c0f0d16169833f7ffaba4574e3bc
Javier <javierLhernandez@yahoo.com>
2024-09-20 21:43:15 -0400

    fixed migration

--- backend/db/migrations/20240920024053-create-user.js
+++ backend/db/migrations/20240920024053-create-user.js
@@ -1,5 +1,4 @@
 'use strict';
-// /** @type {import('sequelize-cli').Migration} */
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
@@ -43,8 +42,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
-    // options.tableName = "Users";
-    // return queryInterface.dropTable(options);
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
   }
 };
