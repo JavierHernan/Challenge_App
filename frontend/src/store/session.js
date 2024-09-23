2d4c49091260c5680cd0c02fa0df426d23363382
Javier <javierLhernandez@yahoo.com>
2024-09-23 19:26:44 -0400

    phase 1 complete

---
+++ frontend/src/store/session.js
@@ -0,0 +1,57 @@
+import { csrfFetch } from './csrf';
+
+//Constants
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
+
+//Action Creators
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user
+  };
+};
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER
+  };
+};
+
+//Thunks
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({
+      credential,
+      password
+    })
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+//Reducer + State Initialization
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER:
+      return { ...state, user: action.payload };
+    case REMOVE_USER:
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
